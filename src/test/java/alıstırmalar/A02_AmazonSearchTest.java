df158f4526f05439fde6b8c838b59b81c964ab27
Esat35 <dugindugin35@gmail.com>
2022-05-14 16:15:59 +0300

    14.05.2022

--- src/test/java/alıstırmalar/A02_AmazonSearchTest.java
+++ src/test/java/alıstırmalar/A02_AmazonSearchTest.java
@@ -1,2 +1,65 @@
-package alıstırmalar;public class A02_AmazonSearchTest {
+package alıstırmalar;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class A02_AmazonSearchTest {
+    //Cass W ork A mazon S earch T est
+    //1 https://www.amazon.com/ sayfasina gidelim
+    //2 arama kutusunu locate edelim
+    //3--“Samsung headphones” ile arama yapalim
+    //4 Bulunan sonuc sayisini yazdiralim
+    //5 Ilk urunu tiklayalim
+    //6 Sayfadaki tum basliklari yazdiralim
+
+    WebDriver driver;
+
+    @Before
+    public  void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public  void down() {
+        driver.close();
+    }
+
+
+
+    @Test
+    public  void test01(){
+        //1 https://www.amazon.com/ sayfasina gidelim
+        driver.get("https://www.amazon.com");
+        //2 arama kutusunu locate edelim
+        WebElement aramakutusuElementi=driver.findElement(By.id("twotabsearchtextbox"));
+
+        //3--“Samsung headphones” ile arama yapalim
+        aramakutusuElementi.sendKeys("Samsung headphones"+ Keys.ENTER);
+
+        //4 Bulunan sonuc sayisini yazdiralim
+        WebElement aramaSonucSayısıElementi=driver.findElement(By.xpath("(//div[@class='sg-col-inner'])[1]"));
+        System.out.println(aramaSonucSayısıElementi.getText());
+        //5 Ilk urunu tiklayalim
+        driver.findElement(By.xpath("(//div[@class='a-section aok-relative s-image-fixed-height'])[1]")).click();
+        //6 Sayfadaki tum basliklari yazdiralim
+      String baslıklar=driver.getTitle();
+        System.out.println("baslıklar = " + baslıklar);
+    }
+
+
+
 }
--- src/test/java/alıstırmalar/A03.java
+++ src/test/java/alıstırmalar/A03.java
@@ -1,2 +1,80 @@
-package alıstırmalar;public class A03 {
+package alıstırmalar;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class A03 {
+    //1 C01_TekrarTesti isimli bir class olusturun
+    //2 https://www.google.com/ adresine gidin
+    //3 cookies uyarisini kabul ederek kapatin
+    //4 Sayfa basliginin “Google” ifadesi icerdigini test edin
+    //5 Arama cubuguna “Nutella” yazip aratin
+    //6 Bulunan sonuc sayisini yazdirin
+    //7 sonuc sayisinin 10 milyon’dan fazla oldugunu test edin
+    //8 Sayfayi kapatin
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void test01() {
+
+        //2 https://www.google.com/ adresine gidin
+        driver.get("https://www.google.com");
+        //3 cookies uyarisini kabul ederek kapatin
+
+        //4 Sayfa basliginin “Google” ifadesi icerdigini test edin
+        String baslık = driver.getTitle();
+        String aranacakDeger = "Google";
+
+        if (baslık.contains(aranacakDeger)) {
+
+            System.out.println("Baslık testı PASSED");
+        } else {
+            System.out.println("Baslık testı FAILED");
+        }
+
+        //5 Arama cubuguna “Nutella” yazip aratin
+        driver.findElement(By.xpath("//input[@title='Ara']")).sendKeys("Nutella" + Keys.ENTER);
+        //6 Bulunan sonuc sayisini yazdirin
+        WebElement sonucSayısı = driver.findElement(By.xpath("//div[@id='result-stats']"));
+        System.out.println("Sonuc sayısı: " + sonucSayısı.getText());
+        //7 sonuc sayisinin 10 milyon’dan fazla oldugunu test edin
+
+        String arr[] = sonucSayısı.getText().split(" ");
+        System.out.println("arr = " + Arrays.toString(arr));
+
+        int sonucSayısı2 = Integer.parseInt(arr[1].replaceAll("\\D", ""));
+        System.out.println("sonuc sayısı : " + sonucSayısı2);
+        if (sonucSayısı2 > 10000000) {
+            System.out.println("Sonuc sayısı twsti PASSED");
+        } else {
+            System.out.println("Sonuc sayısı twsti FAILED");
+        }
+
+        //8 Sayfayi kapatin
+    }
 }
