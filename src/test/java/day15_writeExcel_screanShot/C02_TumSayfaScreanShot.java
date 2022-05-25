494f79936b04c9c62bda9595e1fc333f751c3f14
Esat35 <dugindugin35@gmail.com>
2022-05-25 21:47:18 +0300

    25.05.2022

---
+++ src/test/java/day15_writeExcel_screanShot/C02_TumSayfaScreanShot.java
@@ -0,0 +1,39 @@
+package day15_writeExcel_screanShot;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.TestBase;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class C02_TumSayfaScreanShot extends TestBase {
+    @Test
+    public void tumSayfaScreanShot() throws IOException {
+        //amazon sayfasına gıdıp tum sayfanın fotografını cekelım.
+        driver.get("https://www.amazon.com");
+
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("YYMMddHHmmss");
+
+        LocalDateTime date=LocalDateTime.now();
+      String tarih=  date.format(dtf);
+        System.out.println("tarih = " + tarih);
+
+        TakesScreenshot ts= (TakesScreenshot) driver;
+        File tumSayfaResim=new File("target/ekranGoruntuleri/tumSayfa"+tarih+".jpeg");
+        File geciciResim=ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(geciciResim,tumSayfaResim);
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/day15_writeExcel_screanShot/C03_IstenenWebElementScreanShot.java
@@ -0,0 +1,16 @@
+package day15_writeExcel_screanShot;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C03_IstenenWebElementScreanShot extends TestBase {
+
+    @Test
+    public void webElementScreanShot() {
+
+        //amazona gidip nutella aratalım
+        // ve sonuc sayısının oldugu web elementin fotografını cekelım.
+
+
+    }
+}
